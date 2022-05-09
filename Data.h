64cc192621c255c6546e67209f32509f766fdde6
Vinicius de Oliveira Silva <vinioliveira.s@hotmail.com>
2022-05-09 12:01:12 -0300

    header file for Data struct

---
+++ Data.h
@@ -0,0 +1,13 @@
+// Data the tree holds
+typedef struct Data
+{
+    int info;
+}Data;
+
+void printData(Data data);
+
+void readData(Data *data);
+
+void randomData(Data *data);
+
+void newData(Data *data);
