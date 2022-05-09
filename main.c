931463a896e463d6c3d70e4ed8aa3e468c7afbe0
Vinicius de Oliveira Silva <vinioliveira.s@hotmail.com>
2022-05-09 12:03:31 -0300

    Main file

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "bin_tree.h"
+
+int main(int argc, char const *argv[])
+{
+    Node *root;
+    root = newBinTree(10,1);
+    Data *data;
+    
+    printTree(root, root->height);
+    printf("\n");
+    preOrder(root);
+    printf("\n");
+    inOrder(root);
+    printf("\n");
+    postOrder(root);
+    printf("\n");
+    return 0;
+}
