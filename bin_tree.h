2c2c2da11973a72e968b19dc1a9614e21c06e3a4
Vinicius de Oliveira Silva <vinioliveira.s@hotmail.com>
2022-05-09 12:02:38 -0300

    header file

---
+++ bin_tree.h
@@ -0,0 +1,27 @@
+#include "Data.h"
+
+typedef struct Node
+{
+    struct Node *left;  // left son
+    struct Node *right; // right son
+    Data *data;
+    int height; 
+}Node;
+
+void printTree(Node *root, int level);
+
+int bigger(int a, int b);
+
+int getHeight(Node *node);
+
+void setHeight(Node *root);
+
+Node* newBinTree(int num_node, int random);
+
+void preOrder(Node *root);
+
+void inOrder(Node *root);
+
+void postOrder(Node *root);
+
+
